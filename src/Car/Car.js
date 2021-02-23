5e711fd0d4a4750c1f1804c237715f77a5d7b7cf
Ivan Plyusnin <plyusnin.ivan.v@gmail.com>
2021-02-23 13:55:05 +0300

    Adding Radium package

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Radium from "radium";
 import './Car.css'
 
-export default (props) => {
+
+
+const Car = props => {
     const inputClasses = ['input'];
+
+    const style = {
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        border: '1px solid #ccc',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            cursor: 'pointer'
+        }
+    };
 
     if (props.name !== ''){
         inputClasses.push('green');
@@ -18,7 +31,7 @@ export default (props) => {
 
 
     return(
-        <div className="Car">
+        <div className="Car" style={style}>
             <h2>Car name: {props.name}</h2>
             <p>Year: <strong>{props.year}</strong></p>
             <input
@@ -31,4 +44,7 @@ export default (props) => {
         </div>
 
     )
-}
+};
+
+
+export default Radium(Car)
