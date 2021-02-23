705305f569fd5bea69b524191a6aa2d41943deed
Ivan Plyusnin <plyusnin.ivan.v@gmail.com>
2021-02-23 13:24:02 +0300

    Editing styles

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './Car.css'
 
-export default (props) => (
-    <div>
-        <h2>Car name: {props.name}</h2>
-        <p>Year: <strong>{props.year}</strong></p>
-        <button onClick={props.onChangeTitle}>Click</button>
-    </div>
+export default (props) => {
+    const inputClasses = ['input'];
 
-)
+    if (props.name !== ''){
+        inputClasses.push('green');
+        if (props.name.length > 4){
+            inputClasses.push('bold')
+        }
+
+    } else {
+        inputClasses.push('red')
+    }
+
+
+
+
+    return(
+        <div className="Car">
+            <h2>Car name: {props.name}</h2>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input
+                className={inputClasses.join(' ')}
+                type="text"
+                onChange={props.onChangeName}
+                value={props.name}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+
+    )
+}
